1317df0feb841badad4f285e9e47ff438bf9679f
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-08-23 14:15:21 +0900

    時間入力説明訂正

--- g_kiroku.rb
+++ g_kiroku.rb
@@ -1,15 +1,18 @@
-puts "Ruby学習記録"
-puts "入力15分単位"
+puts "Ruby・Rails学習記録"
+puts "15分で ■１個：15の倍数(例：45)で入力"
 
 def input_time
   input_time = gets.to_i
 end
+
 R_time = []
+puts "Rubyの学習時間は何分？"
 r_kazu = input_time / 15
 R_time.push("Ruby:" + " ■" * r_kazu)
 puts R_time
 
 Rl_time = []
+puts "Railsの学習時間は何分？"
 r_kazu = input_time / 15
 Rl_time.push("Rails:" + " ■" * r_kazu)
 puts Rl_time
